7dd3cb9eb8d3d7e5b81dc8f135530676c78e2c37
igorC5 <igorcandidofs@gmail.com>
2025-04-01 19:21:00 -0300

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import './styles/style.css'
+
+function App() {
+
+  return (
+    <div className='bg-blue-500 h-screen flex justify-center items-center'>
+      <h1 className='text-[500%] center bg-red-50 border-solid border-b-6 border-r-6'>garçom da silva</h1>
+    </div>
+  )
+}
+
+export default App
