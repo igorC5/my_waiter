c95f3c4bf23668f4056d6af97d3d3d361cb941bf
igorC5 <igorcandidofs@gmail.com>
2025-04-01 20:21:07 -0300

    ajuste tela de login

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,21 @@
+import TestButton from './components/TestButton'
 import './styles/style.css'
 
 function App() {
 
   return (
-    <div className='bg-blue-500 h-screen flex justify-center items-center'>
-      <h1 className='text-[500%] center bg-red-50 border-solid border-b-6 border-r-6'>garçom da silva</h1>
+    <div className='bg-blue-500 w-full h-screen mx-auto'>
+      <div className="flex flex-col mx-auto w-[50%]">
+        <div className="mt-50 w-75 h-20 bg-white flex items-center justify-center mx-auto border-b-10 border-r-10">
+          <p className='text-center font-semibold text-[50px]'>garçom</p>
+        </div>
+        <div className="mx-auto">
+          <br></br>
+          <TestButton text_btn={"Cadastro"}></TestButton>
+          <br></br>
+          <TestButton text_btn={"Login"}></TestButton>
+        </div>
+    </div>
     </div>
   )
 }
