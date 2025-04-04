3428851efc98c04bd1d9536cf0a5338532890b5f
igorC5 <igorcandidofs@gmail.com>
2025-04-03 21:51:29 -0300

    changing all

--- src/App.jsx
+++ src/App.jsx
@@ -1,23 +1,16 @@
-import TestButton from './components/TestButton'
-import './styles/style.css'
+import { Link, Route, BrowserRouter as Router, Routes } from 'react-router-dom'; // Importando da biblioteca correta
+import './styles/style.css';
+import LoginPage from './pages/loginPage';
 
 function App() {
 
   return (
-    <div className='bg-blue-500 w-full h-screen mx-auto'>
-      <div className="flex flex-col mx-auto w-[50%]">
-        <div className="mt-50 w-75 h-20 bg-white flex items-center justify-center mx-auto border-b-10 border-r-10">
-          <p className='text-center font-semibold text-[50px]'>garçom</p>
-        </div>
-        <div className="mx-auto">
-          <br></br>
-          <TestButton text_btn={"Cadastro"}></TestButton>
-          <br></br>
-          <TestButton text_btn={"Login"}></TestButton>
-        </div>
-    </div>
-    </div>
-  )
+    <Router>
+        <Routes>
+          <Route path="/" element={<LoginPage />} />
+        </Routes>
+    </Router>
+  );
 }
 
-export default App
+export default App;
--- src/pages/loginPage.jsx
+++ src/pages/loginPage.jsx
@@ -1,7 +1,12 @@
 
-export default LoginPage(){
+export default function LoginPage(){
     return (
-        <>
-        </>
+        <div className="flex bg-black w-[100vw] h-[100vh] justify-center items-center">
+            <button 
+                className="bg-white font-mono text-[25px] w-[10%] h-[9%] hover:text-[35px] hover:bg-black hover:text-white hover:border-white hover:border-1"
+            >
+                Está
+            </button>
+        </div>
     )
 }
